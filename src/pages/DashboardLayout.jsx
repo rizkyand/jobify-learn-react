1218d9bc72f96d0e7959a75c642f4adea763c1f3
rizkyand <anandarizky202@gmail.com>
2023-11-10 15:26:50 +0700

    Setting Docker and Playing with Dashboard

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {createContext, useContext, useState} from 'react';
+import {Outlet} from "react-router-dom";
+import {Navbar, SmallSideBar, BigSideBar} from "../component/index.js";
+import Wrapper from "../assets/wrappers/Dashboard.js";
 
+const DashBoardContext = createContext();
 function DashboardLayout(props) {
+    const userDummy = {name : 'Rizky'};
+    const [showSideBar, setShowSetBar] = useState(false);
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    const toogleSideBar = () => {
+        setShowSetBar(!showSideBar);
+        console.log(!showSideBar);
+    }
+    const toogleDarkTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    }
+
+    const isUserLogout = () => {
+        console.log('logout user');
+    }
+
     return (
-        <h1>Dashboard Pages</h1>
+        <DashBoardContext.Provider
+            value={
+                {userDummy,
+                showSideBar,
+                isDarkTheme,
+                toogleDarkTheme,
+                toogleSideBar,
+                isUserLogout}}>
+        <Wrapper>
+            <main className="dashboard">
+                <SmallSideBar/>
+                <BigSideBar/>
+                <div>
+                    <Navbar/>
+                    <div className="dashboard-page">
+                        <Outlet/>
+                    </div>
+                </div>
+            </main>
+        </Wrapper>
+        </DashBoardContext.Provider>
     );
 }
 
+export const useDashboardContext = () => useContext(DashBoardContext);
 export default DashboardLayout;
